d6129e9ce80a1aeafff510dec96907346a695077
Amanda Lemos Ribas <amandaribas@id.uff.br>
2024-11-29 18:42:46 -0300

    enemy

--- labDeJogos/spaceInvanders/game.py
+++ labDeJogos/spaceInvanders/game.py
@@ -4,6 +4,9 @@ from PPlay.sprite import *
 from PPlay.gameimage import *
 import menu
 
+lMonstro = 3
+cMonstro = 7
+velEnemy = 500
 def posInicialTiro(player,shots):
     shot = Sprite("images/shot.png")
     shot.x = player.x + player.width/2 - shot.width/2  
@@ -11,43 +14,92 @@ def posInicialTiro(player,shots):
     shots.append(shot)
     return shots
 
+def criaMatriz(matriz):
+    yInicial = 100
+    for i in range(lMonstro):
+        distanciaX = 0
+        xInicial = 200
+        linha = []
+        for j in range (cMonstro):
+            enemy = Sprite("images/enemy1.png")
+            enemy.x = xInicial + distanciaX
+            enemy.y = yInicial + i*150
+            distanciaX += enemy.width + enemy.width/2
+            linha.append(enemy)
+        matriz.append(linha)
+    return matriz
+
+def desenhaMatriz(matriz):
+    for linha in matriz:
+        for sprite in linha:
+            sprite.draw()
+
+def updateMonstro(matriz, janela, velocidade):
+    bateu = False
+    vel = velocidade * janela.delta_time()
+
+    # Verificar colisões e mover os sprites
+    for linha in matriz:
+        for sprite in linha:
+            sprite.x -= (velocidade * janela.delta_time())  # Aplica o movimento
+            if (sprite.x < 10):
+                sprite.x = 10
+                bateu = True
+            elif (sprite.x >= janela.width - sprite.width - sprite.width/2):
+                sprite.x = janela.width - sprite.width - sprite.width/2
+                bateu = True
+
+    # Inverter direção se necessário
+    if bateu:
+        velocidade *= -1
+        for linha in matriz:
+            for sprite in linha:
+                sprite.x -= (velocidade * janela.delta_time())              
+
 def game(dif = 1):
-    janela = Window(1200, 844)
+    janela = Window(1200, 744)
     teclado = Window.get_keyboard()
     player = Sprite("images/player.png")
     player.x = (janela.width)/2 - player.width/2
-    player.y = 744
+    player.y = 644
     vel = 500
     shots = []
-    tiros = 0
+    enemys = []
+    contaTiros = 0
+    enemys = criaMatriz(enemys)
     while True:
         janela.set_background_color([0,0,0])
+
 
         #movendo player
         if (teclado.key_pressed("right") and player.x <= (janela.width - player.width)) or (teclado.key_pressed("left") and player.x >= 0):
             player.move_key_x(vel * janela.delta_time())
 
         #tiros do player c/ delay
-        if tiros >1: 
-            if (teclado.key_pressed("space")) and shots[tiros-1].y < 500:
+        if contaTiros >1: 
+            if (teclado.key_pressed("space")) and shots[contaTiros-1].y < 500:
                 shots = posInicialTiro(player,shots)
-                tiros += 1
+                contaTiros += 1
         else: #se eh o primeiro, nao precisa de delay
             if (teclado.key_pressed("space")):
                 shots = posInicialTiro(player,shots)
-                tiros += 1
+                contaTiros += 1
     
-        if len(shots) > 0:
-            for balas in shots:
-                balas.draw()
-                if balas.y > 0:
-                    balas.y -= vel * janela.delta_time()
-                if balas.y == 0:
-                    shots.remove(balas)
-                    tiros -= 1
+        if len(shots) > 0: #se atirou
+            for bala in shots:
+                bala.draw()
+                if bala.y > 0:
+                    bala.y -= vel * janela.delta_time()
+                if bala.y < 10:
+                    shots.remove(bala)
+                    contaTiros -= 1
+
        #volta pro menu         
         if (teclado.key_pressed("esc")):
             menu.menu()
+        updateMonstro(enemys,janela,velEnemy)        
+        desenhaMatriz(enemys)
         player.draw()
         janela.update()
 
+game()
--- labDeJogos/spaceInvanders/menu.py
+++ labDeJogos/spaceInvanders/menu.py
@@ -6,7 +6,7 @@ import menuDificuldade
 import game
 
 def menu():
-    janela = Window(1200, 844)
+    janela = Window(1200, 744)
     background = GameImage("images/background.png")
     iniciar_buttom = GameImage("buttom/iniciar.png")
     dif_buttom = GameImage("buttom/dificuldade.png")
@@ -16,20 +16,20 @@ def menu():
     mouse = Window.get_mouse()
 
     #posicao Incicial
-    iniciar_buttom.set_position(350,150)
-    dif_buttom.set_position(350,320)
-    rank_buttom.set_position(350,490)
-    sair_buttom.set_position(350,660)
+    iniciar_buttom.set_position(350,100)
+    dif_buttom.set_position(350,270)
+    rank_buttom.set_position(350,440)
+    sair_buttom.set_position(350,610)
 
 
     while True:
         janela.set_title("MENU")
-        if (mouse.is_over_area([350,150], [800,250])) and mouse.is_button_pressed(1): #iniciar
+        if (mouse.is_over_area([350,100], [800,200])) and mouse.is_button_pressed(1): #iniciar
             game.game()
-        if(mouse.is_over_area([350,320], [800,420])) and mouse.is_button_pressed(1): #dificuldade
+        if(mouse.is_over_area([350,270], [800,370])) and mouse.is_button_pressed(1): #dificuldade
             menuDificuldade.menuDif()
         #if(mouse.is_over_area([350,490], [800,590])) and mouse.is_button_pressed(1): #ranking
-        if(mouse.is_over_area([350,660], [800,760])) and mouse.is_button_pressed(1): #sair
+        if(mouse.is_over_area([350,440], [800,540])) and mouse.is_button_pressed(1): #sair
             janela.close()
 
         background.draw()
